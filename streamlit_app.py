d5ebc911b2544868aa8f56e15d0708e02ec6aff8
mickitty0511 <y.reijun1096@gmail.com>
2023-08-25 18:15:55 +0900

    modified alias for packages

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
 
 # New Section to display fruityvice api response
-streamlit.header("Fruityvice Fruit Advice!")
+st.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+st.text(fruityvice_response.json())
