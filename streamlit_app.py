7f8d132fdec9eb72edc6ac9d90dbac4303d8f16e
mickitty0511 <y.reijun1096@gmail.com>
2023-08-25 19:44:07 +0900

    modified calling packages

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 st.dataframe(fruityvice_normalized)
 
 import snowflake.connector as sf
-my_cnx = sf.connector.connect(**st.secrets["snowflake"])
+my_cnx = sf.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
