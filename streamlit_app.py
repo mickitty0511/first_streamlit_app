011caf049cd5ca671a679f84bb7ce7d459d8cc2d
mickitty0511 <y.reijun1096@gmail.com>
2023-08-25 11:05:27 +0900

    commeted out codes

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,18 +10,18 @@ st.text('🥑🍞 Avocado Toast')
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas as pd
+# import pandas as pd
 
-my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+# my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = st.multiselect(
-     "Pick some fruits:"
-     , list(my_fruit_list.index)
-     , ['Avocado', 'Strawberries']     
-)
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+# # Let's put a pick list here so they can pick the fruit they want to include 
+# fruits_selected = st.multiselect(
+#      "Pick some fruits:"
+#      , list(my_fruit_list.index)
+#      , ['Avocado', 'Strawberries']     
+# )
+# fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-# Display the table on the page.
-st.dataframe(fruits_to_show)
+# # Display the table on the page.
+# st.dataframe(fruits_to_show)
